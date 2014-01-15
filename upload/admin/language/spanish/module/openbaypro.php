f8c0fe07474de03310d8975436c8e7702489d48f
rowend <rowend07@gmail.com>
2014-01-14 22:44:13 -0600

    module folder

---
+++ upload/admin/language/spanish/module/openbaypro.php
@@ -0,0 +1,6 @@
+<?php
+$_['heading_title'] = 'OpenBay Pro';
+
+$_['text_module'] = 'Modulos';
+$_['text_installed'] = 'El modulo de OpenBay Pro esta instalado. Esta disponible bajo Extensiones -> OpenBay Pro';
+?>
