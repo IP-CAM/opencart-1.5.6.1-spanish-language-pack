4ce111606bb2956607d8fc1d6115a0f6bcc401b4
rowend <rowend07@gmail.com>
2014-01-14 22:36:53 -0600

    extension folder

---
+++ upload/admin/language/spanish/extension/default.php
@@ -0,0 +1,16 @@
+<?php
+$_['lang_openbay_new']              = 'Crear Nuevo listado';
+$_['lang_openbay_edit']             = 'Ver / Editar listado';
+$_['lang_openbay_fix']              = 'Arreglar errores';
+$_['lang_openbay_processing']       = 'Procesar';
+
+$_['lang_amazonus_saved']           = 'Guardar (no subido)';
+$_['lang_amazon_saved']             = 'Guardar (no subido)';
+
+$_['lang_markets']                  = 'Mercados';
+$_['lang_bulk_btn']                 = 'eBay subida de volumenes';
+$_['lang_bulk_amazon_btn']          = 'Amazon EU subida de volumenes';
+
+$_['lang_marketplace']              = 'Mercados';
+$_['lang_status']                   = 'Estatus';
+$_['lang_option']                   = 'Opción';
