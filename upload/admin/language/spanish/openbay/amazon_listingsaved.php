1ab8c77c032b5987726a682825a94eb4a9b2132e
rowend <rowend07@gmail.com>
2014-01-19 18:19:36 -0600

    new files for traslation

---
+++ upload/admin/language/spanish/openbay/amazon_listingsaved.php
@@ -0,0 +1,18 @@
+<?php
+$_['lang_title']                    = 'OpenBay Pro para Amazon | Listados Guardados';
+$_['lang_saved_listings']           = 'Listados Guardados';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_overview']                 = 'Vista General de Amazon';
+$_['lang_btn_return']               = 'Cancelar';
+$_['lang_description']              = 'Esta es la lista de los listados de los productos que estan guardados localmente y que estan listos para ser subido a Amazon. Da Click en subir para publicar.';
+$_['lang_actions_edit']             = 'Editar';
+$_['lang_actions_remove']           = 'Remover';
+$_['lang_btn_upload']               = 'Subir';
+$_['lang_name_column']              = 'Nombre';
+$_['lang_model_column']             = 'Modelo';
+$_['lang_sku_column']               = 'SKU';
+$_['lang_amazon_sku_column']        = 'SKU del Artículo de Amazon';
+$_['lang_actions_column']           = 'Acción';
+$_['lang_uploaded_alert']           = 'Listado Guardados fueron subidos!';
+$_['lang_delete_confirm']           = 'Estas Seguro?';
+?>
---
+++ upload/admin/language/spanish/openbay/amazon_overview.php
@@ -0,0 +1,15 @@
+<?php
+
+$_['lang_title'] = 'OpenBay Pro para Amazon';
+$_['lang_heading'] = 'Vista General de Amazon';
+$_['lang_overview'] = 'Vista General de Amazon';
+$_['lang_openbay'] = 'OpenBay Pro';
+$_['lang_heading_settings'] = 'Configuraciones';
+$_['lang_heading_account'] = 'Mi cuenta';
+$_['lang_heading_links'] = 'Enlaces del artículo';
+$_['lang_heading_bulk_listing'] = 'Listado en masa';
+$_['lang_heading_register'] = 'Registrar';
+$_['lang_heading_stock_updates'] = 'Actualizaciones del Stock';
+$_['lang_heading_saved_listings'] = 'Listados Guardados';
+$_['lang_heading_bulk_linking'] = 'Enlazado en masa';
+?>
