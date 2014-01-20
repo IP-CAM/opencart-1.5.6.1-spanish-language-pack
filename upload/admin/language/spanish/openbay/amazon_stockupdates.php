75d5274d1e1306024090779b39aba09004faba77
rowend <rowend07@gmail.com>
2014-01-19 18:50:35 -0600

    other file

---
+++ upload/admin/language/spanish/openbay/amazon_stockupdates.php
@@ -0,0 +1,24 @@
+<?php
+//Headings
+$_['lang_title']                    = 'OpenBay Pro para Amazon | Stock updates';
+
+//Text
+$_['lang_stock_updates']            = 'Actualizaciones del Stock';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_overview']                 = 'Vista General de Amazon';
+$_['lang_my_account']               = 'Mi cuenta';
+$_['lang_btn_return']               = 'Cancelar';
+
+//Table columns
+$_['lang_ref']                      = 'Ref';
+$_['lang_date_requested']           = 'Fecha requerida';
+$_['lang_date_updated']             = 'Fecha actualizada';
+$_['lang_status']                   = 'Estatus';
+$_['lang_sku']                      = 'Amazon SKU';
+$_['lang_stock']                    = 'Stock';
+
+$_['lang_empty']                    = 'No hay resultados!';
+$_['lang_date_start']               = 'Fecha de Inicio:';
+$_['lang_date_end']                 = 'Fecha de Fin:';
+$_['lang_filter_btn']               = 'Filtro';
+?>
