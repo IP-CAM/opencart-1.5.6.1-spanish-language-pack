99c8cc0f4358f96dc7e4239e7d9924a343ad113f
rowend <rowend07@gmail.com>
2014-01-26 13:56:52 -0600

    another file

---
+++ upload/admin/language/spanish/openbay/amazonus_listingsaved.php
@@ -0,0 +1,18 @@
+<?php
+$_['lang_title']                    = 'OpenBay Pro para Amazon US | Listados Guardado';
+$_['lang_saved_listings']           = 'Listados Guardados';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_overview']                 = 'Vista General de Amazonus';
+$_['lang_btn_return']               = 'Cancelar';
+$_['lang_description']              = 'Esta es la lista de listado de productos que esta guardados localmente y están listos para ser subido  Amazon US. Click en subir para publicar.';
+$_['lang_actions_edit']             = 'Editar';
+$_['lang_actions_remove']           = 'Remover';
+$_['lang_btn_upload']               = 'Subir';
+$_['lang_name_column']              = 'Nombre';
+$_['lang_model_column']             = 'Modelo';
+$_['lang_sku_column']               = 'SKU';
+$_['lang_amazonus_sku_column']      = 'SKU del artículo de Amazon';
+$_['lang_actions_column']           = 'Acción';
+$_['lang_uploaded_alert']           = 'Listados guardados subidos!';
+$_['lang_delete_confirm']           = 'Estas seguro?';
+?>
