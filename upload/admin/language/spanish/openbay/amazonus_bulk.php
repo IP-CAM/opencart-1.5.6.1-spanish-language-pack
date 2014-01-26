25e09ad1ec94bac8544c1a5f599a83bbd09f261c
rowend <rowend07@gmail.com>
2014-01-26 12:01:47 -0600

    amazon_bulk

---
+++ upload/admin/language/spanish/openbay/amazonus_bulk.php
@@ -0,0 +1,48 @@
+<?php
+
+$_['heading_title'] = 'OpenBay Pro para Amazon US | Listado en Masa';
+
+$_['text_openbay'] = 'OpenBay Pro';
+$_['text_overview'] = 'Vista general para Amazon US';
+$_['text_bulk_listing'] = 'Listado en Masa';
+$_['text_searching'] = 'Buscando';
+$_['text_finished'] = 'Terminado';
+$_['text_marketplace'] = 'Mercado';
+$_['text_de'] = 'Alemania';
+$_['text_fr'] = 'Francia';
+$_['text_es'] = 'España';
+$_['text_it'] = 'Italia';
+$_['text_uk'] = 'Reino Unido';
+$_['text_filter_results'] = 'Filtrar Resultados';
+$_['text_dont_list'] = 'No listar';
+$_['text_listing_values'] = 'Listando valores';
+$_['text_condition'] = 'Condición';
+$_['text_condition_note'] = 'Nota de la condición';
+$_['text_start_selling'] = 'Empezar a vender';
+$_['text_new'] = 'Nuevo';
+$_['text_used_like_new'] = 'Usado - Como Nuevo';
+$_['text_used_very_good'] = 'Usado - Muy Bueno';
+$_['text_used_good'] = 'Usado - Bueno';
+$_['text_used_acceptable'] = 'Usado - Aceptable';
+$_['text_collectible_like_new'] = 'Coleccionable - Como Nuevo';
+$_['text_collectible_very_good'] = 'Coleccionable - Muy Bueno';
+$_['text_collectible_good'] = 'Coleccionable - Bueno';
+$_['text_collectible_acceptable'] = 'Coleccionable - Aceptable';
+$_['text_refurbished'] = 'Restaurado';
+
+$_['column_name'] = 'Nombre';
+$_['column_image'] = 'Imagen';
+$_['column_model'] = 'Modelo';
+$_['column_status'] = 'Estatus';
+$_['column_matches'] = 'Empata cont';
+$_['column_result'] = 'Resultado';
+
+$_['button_return'] = 'Regresar';
+$_['button_list'] = 'Listar';
+$_['button_search'] = 'Buscar';
+
+$_['error_product_sku'] = 'Los productos deben tener un SKU';
+$_['error_product_no_searchable_fields'] = 'El producto debe tener uno de los siguientes campos llenos ISBN, EAN, UPC o JAN';
+$_['error_bulk_listing_not_allowed'] = 'El listado en masa no esta permitido. Tu cuenta debe ser minimo de medio plan';
+
+?>
