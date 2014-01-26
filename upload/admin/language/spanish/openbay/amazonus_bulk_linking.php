607a6c92f8e9baea360ee57151716f23767384c2
rowend <rowend07@gmail.com>
2014-01-26 12:08:30 -0600

    amazonus_bulk_linking

---
+++ upload/admin/language/spanish/openbay/amazonus_bulk_linking.php
@@ -0,0 +1,29 @@
+<?php
+
+$_['heading_title'] = 'Listado en Masa';
+
+$_['button_return'] = 'Regresar';
+$_['button_load'] = 'Cargar';
+$_['button_link'] = 'Enlace';
+
+$_['text_openbay'] = 'OpenBay Pro';
+$_['text_overview'] = 'Vista General de Amazon';
+$_['text_amazon'] = 'Amazon';
+$_['text_local'] = 'Local';
+$_['text_bulk_linking'] = 'Listado en Masa';
+$_['text_load_listings'] = 'Cargar tus listados desde Amazon';
+$_['text_load_listings_help'] = "Cargar todas tus listas desde Amazon puede tomar algo de tiempo (Hasta 2 horas ). Si tu enlazas tus articulos , Los niveles del stock en Amazon seran actualizados con los niveles del stock de tu tienda.";
+$_['text_loading_listings'] = 'Listados han empezado a ser cargados desde Amazon';
+$_['text_report_requested'] = 'Exito en la petición de listado desde Amazon';
+$_['text_report_request_failed'] = 'No se puede cargar el reporte del listado';
+
+$_['column_asin'] = "ASIN";
+$_['column_price'] = "Precio";
+$_['column_name'] = "Nombre";
+$_['column_sku'] = "SKU";
+$_['column_quantity'] = "Cantidad";
+$_['column_combination'] = "Combinación";
+
+$_['error_bulk_linking_not_allowed'] = 'El listado en masa no esta permitido. Tu cuenta debe ser minimo Plan medio';
+
+?>
