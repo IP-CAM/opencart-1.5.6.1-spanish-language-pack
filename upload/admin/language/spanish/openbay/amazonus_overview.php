908f36f75cbeb7a49b7749e2b2b99b21fe5dd7b4
rowend <rowend07@gmail.com>
2014-01-26 13:58:42 -0600

    details

---
+++ upload/admin/language/spanish/openbay/amazonus_overview.php
@@ -0,0 +1,15 @@
+<?php
+
+$_['lang_title'] = 'OpenBay Pro para Amazon US';
+$_['lang_heading'] = 'Vista general de Amazon US';
+$_['lang_overview'] = 'Vista general de Amazon US';
+$_['lang_openbay'] = 'OpenBay Pro';
+$_['lang_heading_settings'] = 'Configuraciones';
+$_['lang_heading_account'] = 'Mi cuenta';
+$_['lang_heading_links'] = 'Enlaces del Artículo';
+$_['lang_heading_register'] = 'Registrar';
+$_['lang_heading_bulk_listing'] = 'Listado en Masa';
+$_['lang_heading_stock_updates'] = 'Actualizaciones del Stock';
+$_['lang_heading_saved_listings'] = 'Listados Guardados';
+$_['lang_heading_bulk_linking'] = 'Listado en masa';
+?>
