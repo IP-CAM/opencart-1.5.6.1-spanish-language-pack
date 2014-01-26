649ff26a2e26e04a00f6cf457ebdc1cb5fa8713c
rowend <rowend07@gmail.com>
2014-01-26 14:10:32 -0600

    some files

---
+++ upload/admin/language/spanish/openbay/amazonus_stockupdates.php
@@ -0,0 +1,25 @@
+<?php
+//Headings
+$_['lang_title']                    = 'OpenBay Pro para Amazon US | Actualizaciones de Stock';
+
+//Text
+$_['lang_stock_updates']            = 'Actualizaciones de Stock';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_overview']                 = 'Vista general de Amazon US';
+$_['lang_my_account']               = 'Mi cuenta';
+$_['lang_btn_return']               = 'Cancelar';
+
+//Table columns
+$_['lang_ref']                      = 'Ref';
+$_['lang_date_requested']           = 'Fecha de la petición';
+$_['lang_date_updated']             = 'Fecha de actualización';
+$_['lang_status']                   = 'Estatus';
+$_['lang_sku']                      = 'SKU de Amazonus';
+$_['lang_stock']                    = 'Stock';
+
+//Table headings
+$_['lang_empty']                    = 'No hay resultados!';
+$_['lang_date_start']               = 'Fecha de empiezo:';
+$_['lang_date_end']                 = 'fecha de terminación:';
+$_['lang_filter_btn']               = 'Filtrar';
+?>
