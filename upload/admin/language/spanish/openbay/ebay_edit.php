121cb69656005448b99c8bd74c51f0b444dadfd0
rowend <rowend07@gmail.com>
2014-02-02 13:31:17 -0600

    ebay_edit

---
+++ upload/admin/language/spanish/openbay/ebay_edit.php
@@ -0,0 +1,37 @@
+<?php
+$_['lang_title']                    = 'OpenBay Pro';
+$_['lang_page_title']               = 'Revisar los listados de eBay';
+$_['lang_home']                     = 'Principal';
+$_['lang_save']                     = 'Guardar';
+$_['lang_ebay']                     = 'eBay';
+$_['lang_pageaction']               = 'Revisar Listado';
+$_['lang_view']                     = 'Ver listado';
+$_['lang_remove']                   = 'Remover enlace';
+$_['lang_end']                      = 'Terminar listado';
+$_['lang_cancel']                   = 'Cancelar';
+$_['lang_loading']                  = 'Obteniendo la información del artículo desde eBay';
+$_['lang_retry']                    = 'Volver a Intentar';
+$_['lang_error_loading']            = 'Hubo un error obteniendo la información desde eBay';
+$_['lang_saved']                    = 'El listado ha sido guardado';
+$_['lang_tbl_title']                = 'Titulo';
+$_['lang_tbl_price']                = 'Precio<span class="help">Incluye impuestos</span>';
+$_['lang_tbl_qty_instock']          = 'Nivel local del stock<span class="help">Este es el nivel del stock en tu OpenCart</span>';
+$_['lang_tbl_qty_listed']           = 'Cantidad en eBay<span class="help">Este es el nivel actual del stock en eBay</span>';
+$_['lang_tbl_qty_reserve']          = 'Reservar nivel<span class="help">Este es el nivel maximo del stock en eBay (0 = no hay limite de reserva)</span>';
+$_['lang_alert_removed']            = 'Al listado se le ha quitado el enlace';
+$_['lang_alert_ended']              = 'El listado ha sido terminado en eBay';
+$_['lang_confirm']                  = 'Estas seguro?';
+
+$_['lang_stock_matrix_active']      = 'Matrix del Stock (activo)';
+$_['lang_stock_matrix_inactive']    = 'Matix del Stock (no active)';
+$_['lang_stock_col_code']           = 'Código Var / SKU';
+$_['lang_stock_col_listed']         = 'Listado';
+$_['lang_stock_col_limit']          = 'Limite<span class="help">0 = no limite</span>';
+$_['lang_stock_col_qty_total']      = 'En stock';
+$_['lang_stock_col_price']          = 'Precio';
+$_['lang_stock_col_active']         = 'Activo';
+$_['lang_stock_col_add']            = 'Agregar <span class="help">Debe estar activo en OpenCart</span>';
+$_['lang_stock_col_comb']           = 'Combinación';
+$_['lang_error_ended']              = 'Los listados enlazados han terminado, no te sera posible editarlos. Debes remover el enlace.';
+$_['lang_error_reserve_size']       = 'Tu no puedes reservar mayor que tu stock local';
+?>
