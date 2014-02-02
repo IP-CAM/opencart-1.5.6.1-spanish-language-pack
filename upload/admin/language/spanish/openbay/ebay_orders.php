6a803ddd025fbafc1662e91cffda7371381bda00
rowend <rowend07@gmail.com>
2014-02-02 17:57:03 -0600

    new files

---
+++ upload/admin/language/spanish/openbay/ebay_orders.php
@@ -0,0 +1,14 @@
+<?php
+$_['lang_openbay']                      = 'OpenBay Pro';
+$_['lang_page_title']                   = 'OpenBay Pro para eBay';
+$_['lang_ebay']                         = 'eBay';
+$_['lang_heading']                      = 'Importar Pedidos';
+$_['lang_btn_return']                   = 'Regresar';
+$_['lang_sync_orders']                  = 'Pedidos';
+$_['lang_sync_pull_orders']             = 'Jalar nuevos pedidos';
+$_['lang_sync_pull_orders_text']        = 'Jalar pedidos';
+$_['lang_ajax_load_error']              = 'No se pudo obtener una conexión';
+$_['lang_error_validation']             = 'Necesitas registrar el token de tu API y habilitar el modulo.';
+$_['lang_sync_pull_notice']             = 'Esto jalara nuevos pedidos desde la ultima actualización automatica. Si tu ya la has instalado entonces solo sera de manera automatica dentro de 24 horas.';
+$_['lang_ajax_orders_import']           = 'Cualquier nuevo pedido debe aparecer dentro de pocos minutos';
+?>
---
+++ upload/admin/language/spanish/openbay/ebay_overview.php
@@ -0,0 +1,17 @@
+<?php
+$_['lang_title']                    = 'OpenBay Pro para eBay';
+$_['lang_heading']                  = 'Vista general de eBay';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_heading_settings']         = 'Configuraciones';
+$_['lang_heading_sync']             = 'Sincronizar';
+$_['lang_heading_account']          = 'Mi cuenta';
+$_['lang_heading_links']            = 'Enlaces del artículo';
+$_['lang_heading_item_import']      = 'Importar artículos';
+$_['lang_heading_order_import']     = 'Importar pedidos';
+$_['lang_heading_adds']             = 'Add-ons instalados';
+$_['lang_heading_summary']          = 'Resumen de eBay';
+$_['lang_heading_profile']          = 'Perfiles';
+$_['lang_heading_template']         = 'Templates';
+$_['lang_heading_ebayacc']          = 'Cuenta de eBay';
+$_['lang_heading_register']         = 'Registrar aqui';
+?>
