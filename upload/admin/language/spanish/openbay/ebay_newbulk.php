d9a46a0e485907d468298a362e96b40b63fb0191
rowend <rowend07@gmail.com>
2014-02-02 17:47:08 -0600

    new file

---
+++ upload/admin/language/spanish/openbay/ebay_newbulk.php
@@ -0,0 +1,68 @@
+<?php
+//Headings
+$_['lang_page_title']               = 'Listado en masa';
+
+//Buttons
+$_['lang_cancel']                   = 'Cancelar';
+$_['lang_none']                     = 'Ninguno';
+$_['lang_preview']                  = 'Adelanto';
+$_['lang_add']                      = 'Agregar';
+$_['lang_remove']                   = 'Remover';
+$_['lang_save']                     = 'Guardar';
+$_['lang_preview_all']              = 'Verificar todos';
+$_['lang_view']                     = 'Ver listado';
+$_['lang_edit']                     = 'Editar';
+$_['lang_submit']                   = 'Enviar';
+$_['lang_features']                 = 'Editar caracteristicas';
+$_['lang_catalog']                  = 'Seleccionar catalogo';
+$_['lang_catalog_search']           = 'Buscar catalogo';
+
+//Form options / text
+$_['lang_pixels']                   = 'Pixeles';
+$_['lang_yes']                      = 'Si';
+$_['lang_no']                       = 'No';
+$_['lang_other']                    = 'Otro';
+$_['lang_select']                   = 'Seleccionar';
+
+//Profile names
+$_['lang_profile_theme']            = 'Tema del Perfil:';
+$_['lang_profile_shipping']         = 'Perfil de envio:';
+$_['lang_profile_returns']          = 'Perfil de devoluciones:';
+$_['lang_profile_generic']          = 'Perfil Generico:';
+
+//Text
+$_['lang_title']                    = 'Titulo';
+$_['lang_price']                    = 'Precio';
+$_['lang_stock']                    = 'Stock';
+$_['lang_search']                   = 'Buscar';
+$_['lang_loading']                  = 'Cargar detalles';
+$_['lang_preparing0']               = 'Preparando';
+$_['lang_preparing1']               = 'de';
+$_['lang_preparing2']               = 'elementos';
+$_['lang_condition']                = 'Condición:';
+$_['lang_duration']                 = 'Duración:';
+$_['lang_category']                 = 'Categoría:';
+$_['lang_exists']                   = 'Algunos artículos ya están listados en eBay así que han sido removidos';
+$_['lang_error_count']              = 'Has seleccionado %s artículos, puede tomar un momento procesar tus datos';
+$_['lang_verifying']                = 'Verificando artículos';
+$_['lang_processing']               = 'Procesando <span id="activeItems"></span> artículos';
+$_['lang_listed']                   = 'ID del artículo listado: ';
+$_['lang_ajax_confirm_listing']     = 'Estas seguro que quieres listar en masa estos artículos?';
+$_['lang_bulk_plan_error']          = 'tu plan actual no permite subidas en masa, actualiza tu plan<a href="%s">aquí</a>';
+$_['lang_item_limit']               = 'No puedes listar estos artículos por que podrias exceder el limite de tu plan, actualiza tu plan <a href="%s">aquí</a>';
+$_['lang_search_text']              = 'Introduce algún texto de busqueda';
+$_['lang_catalog_no_products']      = 'No se encontraron artículos en tu catalogo';
+$_['lang_search_failed']            = 'La buqueda fallo';
+$_['lang_esc_key']                  = 'La pagina ha sido escondida pero no ha terminado de cargarse';
+
+//Errors
+$_['lang_error_ship_profile']       = 'Necesitas tener un perfil de envios por defecto';
+$_['lang_error_generic_profile']    = 'Necesitas tener un perfil generico';
+$_['lang_error_return_profile']     = 'Necesitas tener un perfil de devolciones';
+$_['lang_error_theme_profile']      = 'Necesitas tener un perfil de temas';
+$_['lang_error_variants']           = 'Artículos con variaciones no pueden ser listados en masa y no han sido seleccionados';
+$_['lang_error_stock']              = 'Algunos artículos no estan en el stock y han sido removidos';
+$_['lang_error_no_product']         = 'No hay productos seleccionados para usar la utilidad de subida en masa';
+$_['lang_error_reverify']           = 'Hubo un error, debes editar y verificar los artículos';
+$_['lang_error_missing_settings']   = 'No puedes listar en masa hasta que sincronices con eBay';
+?>
