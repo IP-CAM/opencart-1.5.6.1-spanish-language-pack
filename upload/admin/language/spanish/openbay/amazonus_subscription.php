3196bcd72130443fea21366aaa6830ada5322c36
rowend <rowend07@gmail.com>
2014-02-02 13:11:24 -0600

    listing data

---
+++ upload/admin/language/spanish/openbay/amazonus_subscription.php
@@ -0,0 +1,27 @@
+<?php
+$_['lang_title']                    = 'OpenBay Pro para Amazon US | Mi cuenta';
+$_['lang_my_account']               = 'Mi cuenta';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_overview']                 = 'Vista General de Amazon US';
+$_['lang_my_account']               = 'Mi Cuenta';
+$_['lang_btn_return']               = 'Cancelar';
+$_['lang_current_plan']             = 'Plan Actual';
+$_['lang_register_invite']          = "No tienes credenciales? Registrate ahora para obtenerlas";
+$_['lang_register']                 = 'Registrar';
+$_['lang_loading']                  = 'Cargando';
+$_['lang_change_plans']             = 'Cambiar Planes';
+$_['lang_your_plan']                = 'Tu plan actual y tu balance actual';
+$_['lang_change_plan']              = 'Cambiar Plan';
+$_['lang_change_plans_help']        = 'No estas satisfecho(a) con tu plan actual?';
+$_['lang_name']                     = 'Nombre';
+$_['lang_description']              = 'Descripción';
+$_['lang_price']                    = 'Precio';
+$_['lang_order_frequency']          = 'Frecuencia de Importación de pedidos';
+$_['lang_product_listings']         = 'Nuevos listados de producto por mes';
+$_['lang_listings_remaining']       = 'Lista de productos restantes';
+$_['lang_listings_reserved']        = 'Los producto han empezado a procesarse';
+$_['lang_account_status']           = 'Estatus de la cuenta';
+$_['lang_merchantid']               = 'ID del mercader';
+$_['lang_change_merchantid']        = 'Cambiar';
+$_['lang_bulk_listing']             = 'Lista en masa';
+?>
