2a4b992ea9d952f268a720d0e844c141778fde0d
rowend <rowend07@gmail.com>
2014-02-02 17:18:14 -0600

    new details

---
+++ upload/admin/language/spanish/openbay/ebay_new.php
@@ -0,0 +1,186 @@
+<?php
+$_['lang_page_title']               = 'Listados de eBay';
+$_['lang_cancel']                   = 'Cancelar';
+$_['lang_none']                     = 'Ninguno';
+$_['lang_preview']                  = 'Avance';
+$_['lang_pixels']                   = 'Pixeles';
+$_['lang_yes']                      = 'Si';
+$_['lang_no']                       = 'No';
+$_['lang_add']                      = 'Agregar';
+$_['lang_remove']                   = 'Remover';
+$_['lang_save']                     = 'Guardar';
+$_['lang_other']                    = 'Otro';
+$_['lang_select']                   = 'Seleccionar';
+$_['lang_loading']                  = 'Cargando';
+$_['lang_confirm_action']           = 'Estas seguro?';
+
+$_['lang_return']                   = 'Regresar a los Productos';
+$_['lang_view']                     = 'Ver listados';
+$_['lang_edit']                     = 'Editar';
+
+$_['lang_catalog_pretext']          = 'Esta petaña mostrara cualquier resultado para artículos encontrados en el catalogo de eBay - seleccionar tu categoría primero.';
+$_['lang_feature_pretext']          = 'Esta petaña mostrara cualquier caracteristica de artículos disponibles - seleccionar tu categoría primero.';
+
+$_['lang_ajax_noload']              = 'No se pudo conectar';
+$_['lang_ajax_catproblem']          = 'Primero debes arreglar los problemas de las categorías antes de listar. Trata de nuevo el el modulo del administrador.';
+$_['lang_ajax_item_condition']      = 'Condición del articulo';
+$_['lang_ajax_error_cat']           = 'Por favor selecciona una categoría de eBay';
+$_['lang_ajax_error_sku']           = 'No se puede enviar un producto sin SKU';
+$_['lang_ajax_error_name']          = 'No se puede enviar un producto sin nombre';
+$_['lang_ajax_error_name_len']      = 'El nombre de l Producto debe estar bajo los 80 caracteres';
+$_['lang_ajax_error_loc']           = 'Introducir le código postal de la localización del artículo';
+$_['lang_ajax_error_time']          = 'Introducir el tiempo del despacho';
+$_['lang_ajax_error_nat_svc']       = 'Agregar almenos un servicio de envío nacional';
+$_['lang_ajax_error_stock']         = 'Tu debes tener un artículo en tu stock para listarlo';
+$_['lang_ajax_error_duration']      = 'Seleccionar la duración de un listado, selecciona una categoría para cargar estas opciones';
+$_['lang_ajax_image_size']          = 'Asegurate que tu tienes una galeria y el tamaño de los thumb';
+$_['lang_ajax_duration']            = 'Seleccionar la duración de un listado';
+$_['lang_ajax_noimages']            = 'Estas seguro que tu no quieres agregar ninguna imagen desde eBay?';
+$_['lang_ajax_mainimage']           = 'Necesitas selccionar una imagen principal desde eBay';
+
+//Tabs
+$_['lang_tab_general']              = 'Categoría';
+$_['lang_tab_feature']              = 'Caracteristicas';
+$_['lang_tab_description']          = 'Descripción';
+$_['lang_tab_images']               = 'Tema &amp; galeria';
+$_['lang_tab_price']                = 'Precio &amp; detalles';
+$_['lang_tab_payment']              = 'Pago';
+$_['lang_tab_shipping']             = 'Envío';
+$_['lang_tab_returns']              = 'Devoluciones';
+
+//Category
+$_['lang_category']                 = 'Categoría';
+$_['lang_category_suggested']       = 'Categoría sugerida de eBay';
+$_['lang_category_suggested_help']  = 'Basado en tu titulo';
+$_['lang_category_suggested_check'] = 'Checando categorías sugeridas con eBay, por favor espera';
+$_['lang_category_popular']         = 'Categorías populares';
+$_['lang_category_popular_help']    = 'Basadas en tu historial';
+$_['lang_category_checking']        = 'Checando los requerimientos de la categoría de eBay, por favor espere ';
+$_['lang_category_features']        = 'Especificaciones del artículo <span class="help">Introduce especificaciones sobre tu artículo que ayudaran a los compradores a buscar los artículos que ellos necesitan . Esto además incrementara el desempeño del producto y la puntuación frl artíulo como mejor valuado.</span>';
+
+//Description
+$_['lang_title']                    = 'Titulo';
+$_['lang_title_error']              = 'Tu titulo tiene que ser de 80 caracteres o menor';
+$_['lang_subtitle']                 = 'Sub titulo';
+$_['lang_subtitle_help']            = 'eBay cargara honorarios extras por subtitulo - <a href="http://pages.ebay.co.uk/help/sell/seller-fees.html" target="_BLANK" onclick="subtitleRefocus();"> click aqui para honorarios</a>';
+$_['lang_template']                 = 'Teme';
+$_['lang_template_link']            = 'Necesitas un tema personalizado?';
+$_['lang_description']              = 'Descripción';
+
+//Images
+$_['lang_images_text_1']            = 'Las imagenes de eBay seran subidas a eBay.<br />Supersize y Gallery plus son listadas como mejoras que podrian costar mas para listar.';
+$_['lang_images_text_2']            = 'Las templates de las imagenes seran agregadas a las descripciones de tus listados y seran organizadas desde tu sitio web, testas son gratis. (Tu template del listado debe tener la etiqueta {gallery})';
+$_['lang_image_gallery']            = 'Tamaño de la imagen de la Galeria';
+$_['lang_image_thumb']              = 'Tamaño de la imagen del Thumb';
+$_['lang_template_image']           = 'Template de la imagen';
+$_['lang_main_image_ebay']          = 'Imagen pricipal de eBay';
+$_['lang_image_ebay']               = 'Imagen de eBay';
+$_['lang_images_none']              = 'No tienes imagenes para este producto';
+$_['lang_images_supersize']         = 'Imagenes super grandes<span class="help">Imagenes extra largas</span>';
+$_['lang_images_gallery_plus']      = 'Gallery plus<span class="help">Imagenes grandes en los resultados de las busquedas</span>';
+
+//Price and details
+$_['lang_listing_condition']        = 'Condición del artículo';
+$_['lang_listing_duration']         = 'Duración del listado';
+$_['lang_listing_1day']             = '1 día';
+$_['lang_listing_3day']             = '3 dias';
+$_['lang_listing_5day']             = '5 dias';
+$_['lang_listing_7day']             = '7 dias';
+$_['lang_listing_10day']            = '10 dias';
+$_['lang_listing_30day']            = '30 dias';
+$_['lang_listing_gtc']              = 'GTC- Bien hasta cancelado';
+$_['lang_stock_matrix']             = 'Matrix del Stock';
+$_['lang_stock_col_qty_total']      = 'En stock';
+$_['lang_stock_col_qty']            = 'Para listar';
+$_['lang_stock_col_qty_reserve']    = 'Reservar';
+$_['lang_stock_col_comb']           = 'Combinación';
+$_['lang_stock_col_price']          = 'Precio';
+$_['lang_stock_col_enabled']        = 'Habilitado';
+$_['lang_qty']                      = 'Cantidad para listar<span class="help">Introducir una cantidad menor si quiere mantener un nivel bajo del stock en eBay</span>';
+$_['lang_price_ex_tax']             = 'Precio ex impuesto';
+$_['lang_price_ex_tax_help']        = 'El precio estandar del artículo sin impuesto. Este valor no sera enviado a eBay.';
+$_['lang_price_inc_tax']            = 'Precio con impuestos';
+$_['lang_price_inc_tax_help']       = 'Este valor no sera enviado a eBayy sera el precio que el usuario pagara.';
+$_['lang_tax_inc']                  = 'Impuesto incluido';
+$_['lang_offers']                   = 'Permitir a los compradores hacer ofertas';
+$_['lang_private']                  = 'Subasta privada<br />(esconder el nombre del comprador)';
+$_['lang_imediate_payment']         = 'Pago inmediato requerido?';
+$_['lang_payment']                  = 'Pagos aceptados';
+$_['lang_payment_pp_email']         = 'PayPal email para pagos:';
+$_['lang_payment_instruction']      = 'Instrucciones de Pago';
+
+//Shipping tab
+$_['lang_item_postcode']            = 'Código postal de la localización<span class="help">Un código postal ayudara a eBay a seleccionar una correcta localización de tus listados</span>';
+$_['lang_item_location']            = 'Pueblo o estado de ubicación<span class="help">Introducir un pueblo es menos importante que un código postal</span>';
+$_['lang_despatch_time']            = 'Dispatch time<span class="help">Max days to send</span>';
+$_['lang_despatch_country']         = 'País del Despacho';
+$_['lang_shipping_national']        = 'Servicios domesticos (nacional)';
+$_['lang_shipping_international']   = 'Servicios Internacionales';
+$_['lang_service']                  = '<strong>Servicio</strong>';
+$_['lang_shipping_cost_first']      = '<strong>Costo</strong>';
+$_['lang_shipping_cost_add']        = '<strong>cada uno adiconal</strong>';
+$_['lang_shipping_post_to']         = '<strong>Publicar en</strong>';
+$_['lang_shipping_post_ww']         = 'Mundial';
+$_['lang_shipping_post_dm']         = 'Domestico y el siguiente';
+$_['lang_shipping_max_national']    = 'Para tu categoría selccionada, eBay te permitira un maximo de cargos de envios nacionales';
+$_['lang_shipping_getitfast']       = 'Obtenlo rapido!';
+
+//Returns
+$_['lang_return_accepted']          = 'devoluciones aceptadas?';
+$_['lang_return_type']              = 'Tipo de devolución';
+$_['lang_return_policy']            = 'Politica de devolución';
+$_['lang_return_days']              = 'Días de devolución';
+$_['lang_return_scosts']            = 'Costos de envio';
+$_['lang_return_restock']           = 'Cuota de Restocking';
+
+
+$_['lang_return_scosts_1']          = 'Los compradores pagan por los envíos de devoluciones';
+$_['lang_return_scosts_2']          = 'El vendedor paga por el envío de la devolución';
+
+//Review page
+$_['lang_review_costs']             = 'Listado de costos';
+$_['lang_review_costs_total']       = 'Total de las cuotas de eBay';
+$_['lang_review_edit']              = 'Editar listado';
+$_['lang_review_preview']           = 'Adelanto del listado';
+$_['lang_review_preview_help']      = '(eBay etiquetas no mostradas)';
+
+//Created
+$_['lang_created_title']            = 'Listado creado';
+$_['lang_created_msg']              = 'Tus listados de eBay han sido creado. El número del artículo de ebay es';
+
+//Failed page
+$_['lang_failed_title']             = 'Listar tu artículo fallo';
+$_['lang_failed_msg1']              = 'Puede haber multples razones para esto.';
+$_['lang_failed_li1']               = 'Si tu eres un vendedor nuevo de eBay ( o no has vendido mucho en el pasado) - necesitaras contactar con eBay para remover tus restricciones de ventas';
+$_['lang_failed_li2']               = 'Tu podrías no estar subscrito a  Selling Manager Pro on eBay - esto es un requerimiento.';
+$_['lang_failed_li3']               = 'Tu cuenta de OpenBay Proesta suspendida, por favor checa en tu modulo del administrador bajo la pestaña de "Mi Cuenta"';
+$_['lang_failed_contact']           = 'Si este error continua , contacta con el soporte despues de que estes seguro que no es ninguno de los problemas anteriores.';
+$_['lang_gallery_select_all']       = 'Selecciona todos<span class="help">marca la casilla para seleccionar todas tus imagenes.</span>';
+$_['lang_template_images']          = 'Imagenes de la template';
+$_['lang_ebay_images']              = 'Imagenes de eBay';
+$_['lang_shipping_in_description']  = 'Información del felte en la descripción<span class="help">SOLO US,UK,AU & CA</span>';
+$_['lang_profile_load']             = 'Cargar perfil';
+$_['lang_shipping_first']           = 'Primer artículo: ';
+$_['lang_shipping_add']             = 'Articulos Adicionales: ';
+$_['lang_shipping_service']         = 'Servicio: ';
+$_['lang_btn_remove']               = 'Remover';
+$_['lang_shop_category']            = 'Categoría de la tienda';
+$_['lang_tab_ebay_catalog']         = 'Catalog de eBay';
+
+//Option images
+$_['lang_option_images']            = 'Variación de imagenes';
+$_['lang_option_images_grp']        = 'Selecciona una opción de grupo';
+$_['lang_option_images_choice']     = 'Imagenes';
+$_['lang_option_description']       = 'variación de imagenes puede ser usado para mostrar una imagen especifica cuando un usuario selecciona una opción. Tu solo puedes usar un conjunto de variación de imagenes pero cada set tiene como maximo 12 imagenes por variación. Las imagenes por default son cargadas desde la opción Catalogo > Opciones';
+
+//Product catalog
+$_['lang_search_catalog']           = 'Buscar en el catalogo de eBay:';
+$_['lang_image_catalog']            = 'Usar la imagen por defautl:<span class="help">Esto cambiara tu imagen principal y sera colocada para usarse en el catalog de eBay</span>';
+
+//Errors
+$_['lang_error_choose_category']    = 'Debes seleccionar una categoría';
+$_['lang_error_enter_text']         = 'Introducir uns texto de busqueda';
+$_['lang_error_no_stock']           = 'No puedes listar un artículo con cero en el stock';
+$_['lang_error_no_catalog_data']    = 'No se encontro datos del catalog de eBay para tu producto';
+$_['lang_error_missing_settings']   = 'No puedes listar artículos hasta que sincronices tus configuraciones de eBay';
+?>
