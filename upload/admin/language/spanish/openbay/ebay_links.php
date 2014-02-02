b24af90f249b7cd04abe1d4dba79411d110ef404
rowend <rowend07@gmail.com>
2014-02-02 14:14:53 -0600

    listados de

---
+++ upload/admin/language/spanish/openbay/ebay_links.php
@@ -0,0 +1,48 @@
+<?php
+// Headings
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_page_title']               = 'OpenBay Pro para eBay';
+$_['lang_ebay']                     = 'eBay';
+$_['lang_heading']                  = 'Enlaces del artículo';
+$_['lang_linked_items']             = 'Articulos enlazados';
+$_['lang_unlinked_items']           = 'Artículos no enlazados';
+
+// Buttons
+$_['lang_btn_resync']               = 'Vorver a sincronizar';
+$_['lang_btn_return']               = 'Regresar';
+$_['lang_btn_save']                 = 'Guardar';
+$_['lang_btn_edit']                 = 'Editar';
+$_['lang_btn_check_unlinked']       = 'Checkar los artículos no enlazados';
+$_['lang_btn_remove_link']          = 'Remover enlace';
+
+// Errors & alerts
+$_['lang_error_validation']         = 'Tu necesitas registrar el token de tu API y habilitar el modulo.';
+$_['lang_alert_stock_local']        = 'Tus listados de eBay serán actualizados cons los nivles de tu stock local!';
+$_['lang_ajax_error_listings']      = 'No se encontrarn productos enlazados';
+$_['lang_ajax_load_error']          = 'No se pudo obtener una respuesta. Trata de nuevo.';
+$_['lang_ajax_error_link']          = 'El enlace del artículo no es valido';
+$_['lang_ajax_error_link_no_sk']    = 'Un enlace no puede ser creado por un artículo que esta agotado en el stock. Termina el artículo de forma manual en eBay.';
+$_['lang_ajax_loaded_ok']           = 'Los artículos han sido cargados con exito';
+
+// Text
+$_['lang_link_desc1']               = 'Enlazando tus artículos te permitira controlar el listado en tu stock de eBay.';
+$_['lang_link_desc2']               = 'Por cada artículo que es actualizado el stock local (el stock disponible en tu tienda OpenCart) actualizara tus listados en eBay';
+$_['lang_link_desc3']               = 'Tu stock local es el stock que que esta disponible para vender. Tus niveles de stock en eBay deberían emptar.';
+$_['lang_link_desc4']               = 'Tu stock asignado son artículos que se han vendido pero no han sido pagados. Estos artículos deberían ser puestos a un lado y no ser tomados en cuenta para calcular tus nivles actuales del stock.';
+$_['lang_text_linked_desc']         = 'Articulos enlazados son artículos de Opencart que tienen una enlace hacia los listados de eBay.';
+$_['lang_text_unlinked_desc']       = 'Los artículos no enlazados son artículos listados en tu cuenta de eBay que not tiene un enlace a ninguno de tus productos en OpenCart.';
+$_['lang_text_unlinked_info']       = 'Da Click en el boton de cehcar los artículos no enlazados para buscar en tus listados activos de eBay. Esto puede tomar mucho tiempo si tu tienes muchos listados en eBay.';
+$_['lang_text_loading_items']       = 'Cargar artículos';
+
+// Tables
+$_['lang_column_action']            = 'Acción';
+$_['lang_column_status']            = 'Estatus';
+$_['lang_column_variants']          = 'Variantes';
+$_['lang_column_itemId']            = 'ID del artículo de eBay';
+$_['lang_column_product']           = 'Producto';
+$_['lang_column_product_auto']      = 'Producto<span class="help">(Autocompleter con el nombre)</span>';
+$_['lang_column_stock_available']   = 'Stock Local<br /><span class="help">Disponibles para vender</span>';
+$_['lang_column_listing_title']     = 'Titulo del listado<span class="help">(titulo del listado en eBay)</span>';
+$_['lang_column_allocated']         = 'Stock asignado<br /><span class="help">Vendido pero no pagado</span>';
+$_['lang_column_ebay_stock']        = 'Stock de eBay<span class="help">En listados</span>';
+?>
